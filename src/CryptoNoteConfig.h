fb2e705c5908ea3a954f526fc9d86f8c10192e50
Blake Gulick <bgulick99@users.noreply.github.com>
2016-12-13 13:29:05 -0600

    Fixed GENESIS_COINBASE_TX_HEX

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "whoviancoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff00011c029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210171414f50f4c5183459ab5a6be0ca35fbac53828b26875ad6a7503f02ecba0eed";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
